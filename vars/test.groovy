e65e84564dd16d8e041e9c932b1ba0bd1ad634b8
Benjamin Toll <benjam72@yahoo.com>
2020-06-06 14:08:34 -0400

    remove pipeline node

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,12 +1,10 @@
 // https://stackoverflow.com/a/54539868
 def call(List<String> targets) {
-    pipeline {
-        node {
-            targets.each {
-                stage (it) {
-                    echo it
-                    sh "ls -l"
-                }
+    node {
+        targets.each {
+            stage (it) {
+                echo it
+                sh "ls -l"
             }
         }
     }
