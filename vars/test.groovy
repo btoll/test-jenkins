b037fd5ab55b374a0a2de5474a0b8a132de464a5
Benjamin Toll <benjam72@yahoo.com>
2020-06-06 14:19:33 -0400

    Added ability to specify git branch

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,12 @@
 // https://stackoverflow.com/a/54539868
 def call(List<String> targets) {
     node {
+        git branch: 'derp', url: 'https://github.com/algorand/indexer.git'
+
         targets.each {
             stage (it) {
                 echo it
-                sh "ls -l"
+                sh "make ${it}"
             }
         }
     }
