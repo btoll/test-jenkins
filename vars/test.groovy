a89de157365f53c29cac31bf2dd66a6bfc8c3f3f
Benjamin Toll <benjam72@yahoo.com>
2020-06-06 14:02:54 -0400

    Try adding pipeline syntax

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,12 @@
 // https://stackoverflow.com/a/54539868
 def call(List<String> targets) {
-    node {
-        targets.each {
-            stage (it) {
-                echo it
-                sh "ls -l"
+    pipeline {
+        node {
+            targets.each {
+                stage (it) {
+                    echo it
+                    sh "ls -l"
+                }
             }
         }
     }
