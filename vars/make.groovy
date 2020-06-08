7b1b2d51b1139ce717ff42f0bd4dd5edee3ceef6
Benjamin Toll <benjam72@yahoo.com>
2020-06-08 16:21:10 -0400

    Removed comment

--- vars/make.groovy
+++ vars/make.groovy
@@ -1,7 +1,6 @@
 // https://stackoverflow.com/a/54539868
 def call(List<String> targets) {
     node {
-//        git branch: 'derp', url: 'https://github.com/algorand/indexer.git'
         checkout scm
 
         docker.build('foo').inside('-u root') {
