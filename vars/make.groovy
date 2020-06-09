3554218a91823fa54e37a39133adcd77812ebe73
Benjamin Toll <benjam72@yahoo.com>
2020-06-09 01:44:11 -0400

    nits

--- vars/make.groovy
+++ vars/make.groovy
@@ -1,10 +1,9 @@
-// https://stackoverflow.com/a/54539868
 def call(List<String> targets) {
     node {
         checkout scm
 
-        withAWS(credentials: 'aws-creds') {
-            docker.build('foo').inside('-u root') {
+        withAWS(credentials: "aws-creds") {
+            docker.build("foo").inside("-u root") {
                 targets.each {
                     stage (it) {
                         sh "make ${it}"
