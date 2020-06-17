cdde89111bd0c797865e05586442b791b86b4c1f
Benjamin Toll <benjam72@yahoo.com>
2020-06-17 18:38:02 -0400

    Added stage in vars/run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,9 +1,11 @@
 //def call(Map config) {
 def call(String target) {
     node {
-        docker.build("foo").inside("-u root") {
-            log.info "Running ${target}"
-            sh "make ${target}"
+        stage("${target}") {
+            docker.build("foo").inside("-u root") {
+                log.info "Running ${target}"
+                sh "make ${target}"
+            }
         }
     }
 }
