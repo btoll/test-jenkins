340f488d3a926197d0820b268d2a12167007d6b5
Benjamin Toll <benjam72@yahoo.com>
2020-10-12 19:12:55 -0400

    futzing around

--- vars/mule.groovy
+++ vars/mule.groovy
@@ -1,17 +1,9 @@
-def call(List<String> targets, String muleYaml="mule.yaml") {
+def call(String recipe) {
     node {
         checkout scm
 
-        docker.build("derp", "-f ./docker/cicd/Dockerfile .").inside("-u root") {
-            targets.each {
-                stage (it) {
-                    if (it == "sign") {
-                        input(message: "Forward gpg-agent")
-                    }
-
-                    sh "mule -f ${muleYaml} ${it}"
-                }
-            }
+        docker.build("mule-test", "-f ./docker/build/docker.ubuntu.Dockerfile .").inside("-u root") {
+            sh "mule -r ${recipe}"
         }
     }
 }
